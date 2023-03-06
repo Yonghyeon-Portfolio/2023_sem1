1a29c91136bd65e95b619b7232eb684a99a3b3c9
isaac_kim <yonghyeon0223@gmail.com>
2023-03-06 23:01:49 +1100

    char * ptr = &arr[0];

--- COMP2017/week2/exercises/e1_variables.c
+++ COMP2017/week2/exercises/e1_variables.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    char character_name[] = "용현";
+    char character_name[] = "Isaac Kim";
+    char* ptr = &character_name[0];
     int character_age = 35;
 
     printf("There once was a man nameD %s\n", character_name);
+    printf("His name starts with %c.\n\n", *ptr);
     printf("He was %d years old\n", character_age);
 
     printf("%ld %ld\n", sizeof(character_name), sizeof(character_age));
